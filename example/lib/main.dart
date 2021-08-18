4147fd2e8d80d98b9d76142dd04b60efd7496662
juliana.dimeglio <juliana.dimeglio@globant.com>
2021-08-18 10:38:52 -0300

    Permission Request Implementation
    Fixed:
    	-Missing constants
    	-Added Logs in functions not implemented

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:android_plugin/android_plugin.dart';
+import 'utils/string_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,34 +13,34 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  bool _permissionGranted = false;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
+  Future<bool> checkPermission() async {
+    bool result = false;
     try {
-      platformVersion =
-          await AndroidPlugin.platformVersion ?? 'Unknown platform version';
+      result = await AndroidPlugin.permissionCheck;
     } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+      print(
+        StringConstants.checkPermissionError,
+      );
     }
+    return _permissionGranted = !result;
+  }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+  Future<void> requestPermission() async {
+    try {
+      await AndroidPlugin.requestPermission;
+    } on PlatformException {
+      print(
+        StringConstants.requestPermissionError,
+      );
+    }
+    return;
   }
 
   @override
@@ -49,10 +48,38 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text(
+            StringConstants.titleAppBar,
+          ),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    checkPermission();
+                  });
+                },
+                child: Text(
+                  StringConstants.checkPermissionButton,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                requestPermission();
+              },
+              child: Text(
+                StringConstants.requestPermissionButton,
+              ),
+            ),
+            Text(
+              _permissionGranted
+                  ? StringConstants.permissionApproved
+                  : StringConstants.permissionDenied,
+            ),
+          ],
         ),
       ),
     );
---
+++ example/lib/utils/string_constants.dart
@@ -0,0 +1,9 @@
+abstract class StringConstants {
+  static const String permissionApproved = "You have permission";
+  static const String permissionDenied = "You don't have permission";
+  static const String requestPermissionButton = "Request permission";
+  static const String checkPermissionButton = "Permission Check";
+  static const String titleAppBar = 'Plugin example app';
+  static const String requestPermissionError = 'No permission';
+  static const String checkPermissionError = "No result";
+}
